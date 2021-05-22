e406d131fd3dc82880ea4fbb09584069845fed46
Rahul Kumar <rahulcsofficial@gmail.com>
2021-05-22 21:29:37 +0530

    Header Added
    Signed-off-by: Rahul Kumar <rahulcsofficial@gmail.com>

---
+++ CryptoCurrency Dashboard/src/App.js
@@ -0,0 +1,14 @@
+import Header from './Header'
+
+function App() {
+  return (
+    
+    <div className="App">
+    
+     <h1>Hello Just Testing</h1>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
